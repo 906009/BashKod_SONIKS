2e9ddc44411253dd12e4fed895e6709d65838db8
906009 <110044475+906009@users.noreply.github.com>
2025-04-28 18:19:18 +0500

    Update rasp.py

--- app/app/api/rasp.py
+++ app/app/api/rasp.py
@@ -112,7 +112,6 @@ async def get_satellite_rasp(stl_id):
                         tle=tle
                     )
                     output.append(obj)
-                output.reverse()
                 return output
     except:
         return ["Нет данных."]
